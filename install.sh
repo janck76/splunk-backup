0f68569427ff80a3281cf391d37fe686029e8c77
Jan <janck76@users.noreply.github.com>
2021-10-12 12:45:38 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+mkdir /opt/backup /opt/backup/bin /opt/backup/log
+chown -R splunk:splunk /opt/backup
+
+cp backup_splunk.sh /opt/backup/bin
+chmod 755 /opt/backup/bin/backup_splunk.sh
+q
+cp backup_splunk.{service,timer} /etc/systemd/system/
+
+systemctl enable backup_splunk.timer
+systemctl start backup_splunk.timer
+
